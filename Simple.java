0d0d9da4782cdd72c1fec83eb1d5c6a4ae1da889
justingywen <145408096+justingywen@users.noreply.github.com>
2024-07-02 11:51:49 +0800

    我的第一支java程式

---
+++ Simple.java
@@ -0,0 +1,10 @@
+/**
+ * 0702
+ */
+public class Simple {
+    public static void main(String[] args) {
+        System.out.println("hello java!");
+        System.out.println("0702");
+    }
+    
+} 
---
+++ ch2_1.java
@@ -0,0 +1,13 @@
+//ch1_2, 簡單的java程式
+public class ch2_1 { //定義public類別ch1_2
+    public static void main(String[] args) { //main()函數,主程式開始
+        int num1=2; //宣告整數num
+        int num2=3;
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are running"); //印出字串及變數內容
+        System.out.println(num2+"dogs are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"tigers are running");
+    }
+}
