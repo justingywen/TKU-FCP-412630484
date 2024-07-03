fdaeeff0d509d22600c9e9ca4a136e9ba45e84e2
justingywen <justinweiheng@gmail.com>
2024-07-03 13:43:06 +0800

    Create hm_02.java

---
+++ hm_02.java
@@ -0,0 +1,7 @@
+public class hm_02 {
+    public static void main(String[] args) {
+        System.out.println("溫偉恆");
+        System.out.println("19歲");
+        System.out.println("興趣是聽音樂");
+    }
+}
