cfdbea45dc70e1d0f3b3a18daa47a9f5a91a614c
justingywen <justinweiheng@gmail.com>
2024-07-03 10:55:01 +0800

    Update ch2_1.java

--- ch2_1.java
+++ ch2_1.java
@@ -1,8 +1,9 @@
 //ch1_2, 簡單的java程式
-public class ch2_1 { //定義public類別ch1_2
+public class ch2_1 { //定義public類別ch2_1
     public static void main(String[] args) { //main()函數,主程式開始
-        int num1=2; //宣告整數num
-        int num2=3;
+        int num1,num2; //宣告整數num
+        num1=1;
+        num2=2;
         float num3=4;
         String num4="五";
         System.out.println(num1+"cats are running"); //印出字串及變數內容
