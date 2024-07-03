f92d0e4335741fe5ce2398d2d41276b27f6f5f96
justingywen <justinweiheng@gmail.com>
2024-07-03 11:39:00 +0800

    Update ch2_4.java

--- ch2_4.java
+++ ch2_4.java
@@ -1,7 +1,9 @@
+//ch2_4有錯誤的程式
 public class ch2_4 {
+
     public static void main(String[] args) {
-        int num1=2;
-        int num2=3;
+        int num1=2; //宣告整數變數num1，並設值為2
+        int num2=3; //宣告整數變數num2，並設值為3
 
         System.out.println("I have "+num1+"dogs");
         System.out.println("You have "+num2+"dogs");
