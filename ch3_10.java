054ea80d4439a4062cd69441a5ff485c39fea0ef
justingywen <justinweiheng@gmail.com>
2024-07-08 14:36:27 +0800

    0708

---
+++ ch3_10.java
@@ -0,0 +1,9 @@
+public class ch3_10 {
+    public static void main(String[] args) {
+        char ch1='\042';
+        char ch2='\u0022';
+        System.out.println("\"Time is money!\"");
+        System.out.println(ch1+"Time files."+ch1);
+        System.out.println(ch2+"Tomottow never comes"+ch2);
+    }
+}
