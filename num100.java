6fb892d041442c551714d50533d45f180b607c9e
justingywen <justinweiheng@gmail.com>
2024-07-15 11:47:01 +0800

    0715

---
+++ num100.java
@@ -0,0 +1,8 @@
+public class num100 {
+    public static void main(String[] args) {
+        int i,sum=0;
+        for(i=1;i<=100;i++)
+            sum+=i;
+        System.out.printf("1+2+....+100=%d",sum);
+    }
+}
