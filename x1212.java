f36a67a255df584254745c99ff0c6f64df488c10
justingywen <justinweiheng@gmail.com>
2024-07-15 13:54:50 +0800

    Create x1212.java

---
+++ x1212.java
@@ -0,0 +1,11 @@
+public class x1212 {
+    public static void main(String[] args) {
+        for(int i=1;i<=12;i++){
+            for(int j=1;j<=12;j++)
+                System.out.printf("%d*%d=%2d ",i,j,i*j);
+            System.out.println();
+        }
+    }
+}
+    
+
